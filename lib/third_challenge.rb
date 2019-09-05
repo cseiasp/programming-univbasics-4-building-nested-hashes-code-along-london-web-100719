08db2fb735ec5245857b6e03c289b3993a1e2f68
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 17:02:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -32,8 +32,8 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {
-        name: "Juliet"
-        age: "15"
+        name: "Juliet",
+        age: "15",
         status: "alive"
         #your key/value pairs here
         },
