fd0e27534781368dbcd2ad7b21ed928a0f389f72
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-05 17:00:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,21 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { 
+        name: "Lord Montague",
+        age: "53"
         #your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Montague",
+        age: "54"
+        
         #your key/value pairs here
         },
       :hero => {
+        name:
+        age:
+        status:
         #your key/value pairs here
         },
       :hero_friends => []
